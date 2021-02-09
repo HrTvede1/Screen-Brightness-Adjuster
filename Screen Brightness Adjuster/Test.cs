9031b0300de5055e73be32756dc27b4eda25be2d
Christian Tvede <ctpotatolol@gmail.com>
2021-02-09 19:56:46 +0100

    Add project files.

---
+++ Screen Brightness Adjuster/Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Management;
+using Windows.Graphics.Display;
+
+
+namespace Screen_Brightness_Adjuster
+{
+    public sealed class BrightnessOverride
+    {
+        public void SetBrightnessLevel(double brightnesslevel, DisplayBrightnessOverrideOptions options)
+        {
+
+        }
+    }
+}
---
+++ Screen Brightness Adjuster/WindowServices.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Management;
+
+namespace Screen_Brightness_Adjuster
+{
+    public partial class WindowsServices : Form
+    {
+        private static ManagementObject _brightnessInstance;
+        private static ManagementBaseObject _brightnessClass;
+        private static int a;
+
+        public WindowsServices()
+        {
+            InitializeComponent();
+        }
+
+        static WindowsServices()
+        {
+            // Querying the Windows service to get the Brightness API.
+            var searcher = new ManagementObjectSearcher(
+                "root\\WMI",
+                "SELECT * FROM WmiMonitorBrightness");
+
+            var results = searcher.Get();
+            var resultEnum = results.GetEnumerator();
+            resultEnum.MoveNext();
+            _brightnessClass = resultEnum.Current;
+
+            // We need to create an instance to use the Set method!
+            var instanceName = (string)_brightnessClass["InstanceName"];
+            _brightnessInstance = new ManagementObject(
+                "root\\WMI",
+                "WmiMonitorBrightnessMethods.InstanceName='" + instanceName + "'",
+                null);
+        }
+
+        public static int GetDeviceCurrentBrightness()
+        {
+            // Getting the current value.
+            var value = _brightnessClass.GetPropertyValue("CurrentBrightness");
+            var valueString = value.ToString();
+            return int.Parse(valueString); // Direct cast fails.
+        }
+
+        public static void SetDeviceBrightness(int newValue)
+        {
+            if (newValue < 0)
+            {
+                newValue = 0;
+            }
+
+            if (newValue > 100)
+            {
+                newValue = 100;
+            }
+
+            var inParams = _brightnessInstance.GetMethodParameters("WmiSetBrightness");
+            inParams["Brightness"] = newValue;
+            inParams["Timeout"] = 0;
+            _brightnessInstance.InvokeMethod("WmiSetBrightness", inParams, null);
+        }
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = trackBar1.Value;
+            SetDeviceBrightness(a);
+        }
+    }
+}
